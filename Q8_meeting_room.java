8d3fb1702611da840aabcba222e8795640088f04
Rijvan <119593250+RIjvan-Juneja@users.noreply.github.com>
2023-06-28 15:09:37 +0530

    Add files via upload

---
+++ Q8_meeting_room.java
@@ -0,0 +1,19 @@
+public class Q8_meeting_room {
+    function canAttendMeetings(intervals) {
+        // Sort the intervals in ascending order of start time
+        intervals.sort((a, b) => a[0] - b[0]);
+
+        // Loop through the intervals and check for overlaps
+        for (let i = 0; i < intervals.length - 1; i++) {
+            // If the end time of the current interval is greater than or equal to
+            // the start time of the next interval, there is an overlap and we can't
+            // attend all the meetings
+            if (intervals[i][1] > intervals[i+1][0]) {
+                return false;
+            }
+        }
+
+        // If there are no overlaps, we can attend all the meetings
+        return true;
+    };
+}
